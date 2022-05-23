2a11c44fb179862da7449da66a7d4b8ac4f086d0
Yoga Febriatala <yogafebriatala24@gmail.com>
2022-05-23 23:59:33 +0700

    constructor

---
+++ bin/constructor.dart
@@ -0,0 +1,11 @@
+class Person {
+  String name = 'Yoga';
+  String? address;
+
+  final String country = 'Indonesia';
+
+  Person(String paramName, String paramAddress) {
+    name = paramName;
+    address = paramAddress;
+  }
+}
